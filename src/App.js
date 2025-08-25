381c9dfa73fd90323569691be12bae66c2872dc6
shravani.gunjal <shravani.gunjal05@gmail.com>
2025-08-25 15:23:36 +0530

    array of object in a table

--- src/App.js
+++ src/App.js
@@ -2,16 +2,31 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const myObject = {
-    name:"Shravani",
-    age:20,
-    college:"B.J.College,Ale"
-  }
+  const student = [
+    {srno:1,name:'Shravani',marks:85},
+    {srno:2,name:'Rutuja',marks:90},
+    {srno:3,name:'Trusha',marks:83},
+    {srno:4,name:'Shardul',marks:81},
+    {srno:5,name:'Gaurav',marks:82},
+    {srno:6,name:'Ayush',marks:80}
+  ];
   return (
    <div>
-    <h1><strong>Name:</strong>{myObject.name}</h1>
-    <h2><strong>Age:</strong>{myObject.age}</h2>
-    <h3><strong>College:</strong>{myObject.college}</h3>
+    <table border="1">
+      <tr>
+        <th>srno</th>
+        <th>name</th>
+        <th>marks</th>
+      </tr>
+      {student.map((singleElement)=>
+      <tr>
+        <td>{singleElement.srno}</td>
+        <td>{singleElement.name}</td>
+        <td>{singleElement.marks}</td>
+      </tr>
+      )}
+    </table>
+
    </div>
   );
 }
